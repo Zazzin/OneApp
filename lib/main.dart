307c03cb195a942864b6572a5576e0f4ee581855
RiccardoGraffii <168535113+RiccardoGraffii@users.noreply.github.com>
2024-05-27 15:45:37 +0200

    added a square

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,32 @@ void main() {
   runApp(const MyApp());
 }
 
+class QuadratoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quadrato in Flutter'),
+        ),
+        body: Center(
+          child: Quadrato(),
+        ),
+      ),
+    );
+  }
+}
+
+class Quadrato extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100.0, // Larghezza del quadrato
+      height: 100.0, // Altezza del quadrato
+      color: Colors.blue, // Colore del quadrato
+    );
+  }
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
